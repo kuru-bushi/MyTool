20a5f6b6ea1d7e089c1e36cea5bd79ec51f58f50
Kunihiro-ikd <kunikuni6129.sddj@gmail.com>
2023-05-21 14:29:06 +0900

    Add: stitching select match or knn

--- stitching/README.md
+++ stitching/README.md
@@ -1,3 +1,9 @@
 # conda info name = stitching
 # 参考
     - https://pystyle.info/opencv-feature-matching/
+
+# trainIdx, queryIdx について
+- distance: 特徴量記述子の距離
+- queryIdx: クエリ記述子 (match(desc1, desc2) と渡した場合、desc1 のインデックス)
+- trainIdx: 学習記述子 (match(desc1, desc2) と渡した場合、desc2 のインデックス)
+
--- stitching/train_query.py
+++ stitching/train_query.py
@@ -4,10 +4,9 @@ import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
 #%%
-origin_path = "./ex.jpg"
-ex_path = "./ex2.jpg"
+origin_path = "./ro1.jpg"
+ex_path = "./ro2.jpg"
 
-#%%
 img = cv.imread(origin_path)
 ex_img = cv.imread(ex_path)
 
@@ -27,26 +26,54 @@ def SIFT(img):
     return kp, desc
 
 # %%
+# get key point, descriptors
 kp1, desc1 = SIFT(img)
 kp2, desc2 = SIFT(ex_img)
 
 # %%
 bf = cv.BFMatcher()
-matches = bf.knnMatch(desc1, desc2, k=2)
+select_match_type = "match" # knn, match
+select_filter = True
 
-good_match = []
-for m, n in matches:
-    if m.distance < 0.7 * n.distance:
-        good_match.append([m])
+def get_good_match(matches, filter=True, thredhold=0.7):
+    if filter:
+        good_match = []
+
+        for m, n in matches:
+            if m.distance < thredhold * n.distance:
+                good_match.append([m])
+        return good_match
+    else:
+        return matches
 
 #%%
-img_pair = cv.drawMatchesKnn(img, kp1, ex_img, kp2, good_match[:10], None, flags=2)
+if select_match_type == "match":
+    matches = bf.match(desc1, desc2)
+    # 
+    good_match = matches
+
+    good_match = sorted(good_match, key = lambda x: x.distance)
+
+    img_pair = cv.drawMatches(img, kp1, ex_img, kp2, good_match[:10], None, flags=2)
+    print(len(good_match))
+
+elif select_match_type == "knn":
+    matches = bf.knnMatch(desc1, desc2, k=2)
+
+    good_match = get_good_match(matches, filter=select_filter)
+
+    good_match = sorted(good_match, key = lambda x: x[0].distance)
+    img_pair = cv.drawMatchesKnn(img, kp1, ex_img, kp2, good_match[:10], None, flags=2)
+    print(len(good_match))
+
 plt.imshow(img_pair)
-
 #%%
-p1, p2 = matches[2]
+# test
+# mat0, mat1 = matches[2]
+# print(mat0.imgIdx, mat0.trainIdx, mat0.queryIdx)
+# print(mat1.imgIdx, mat1.trainIdx, mat1.queryIdx)
 
-print(f"p1: {p1.imgIdx, p1.queryIdx, p1.trainIdx}")
-print(f"p2: {p2.imgIdx, p2.queryIdx, p2.trainIdx}")
+# %%
+print(kp1[0].pt)
 
 # %%
